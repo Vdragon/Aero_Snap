cd605fe55de7f034a01920d91b8d20f70bcb3fd2
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-03-24 21:20:20 +0800

    BASH don't eat UTF8 BOM, make them happy

--- Executables/Snap_max.sh
+++ Executables/Snap_max.sh
@@ -4,9 +4,9 @@
 #Shell script名稱：Snap_max
 #傳回值：0-正常結束
 #已知問題：
-#	
+#
 #修訂紀錄：
-#	
+#
 
 ##將作用中的視窗寬度、高度都調至當前解析度的最大值
 wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
--- Executables/Snap_right.sh
+++ Executables/Snap_right.sh
@@ -4,7 +4,7 @@
 #Shell script名稱：Snap_right
 #傳回值：0-正常結束
 #已知問題：
-#	
+#
 #修訂紀錄：
 #
 ##使用者設定參數##
@@ -15,7 +15,7 @@ WIDTH_OFFSET=7
 
 ##透過分析xdpyinfo回傳的資訊獲取螢幕總寬度的一半
 WIDTH=`xdpyinfo | grep 'dimensions:' | cut -f 2 -d ':' | cut -f 1 -d 'x'`
-WIDTH_HALF=$(($WIDTH/2)) 
+WIDTH_HALF=$(($WIDTH/2))
 
 ##將作用中的視窗寬度、高度接自當前解析度的最大值取消（若有的話）
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
